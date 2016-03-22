bd4e7b45f4b6c03f5a5847199cd4966188df97a9
jminnier <jessica.minnier@gmail.com>
2016-03-21 17:37:29 -0700

    update footer

--- ui.R
+++ ui.R
@@ -33,39 +33,38 @@ customHeaderPanel <- function(title,windowTitle=title){
 }
 # collects all of the tab UIs
 #shinyUI(
-  navbarPage(
-    fluid = TRUE,theme = "bootstrap.min.united.css",
-    #United theme from http://bootswatch.com/
-    #customHeaderPanel(title="START: RNAseq Analysis and Visualization Resource"),#img(src="KCardio_CMYK_4C_pos_small.jpg",height=50,width= 92,align="right")	,
-    title="START: RNAseq Analysis and Visualization Resource", 
-    ## =========================================================================== ##
-    ## DOWNLOAD DATA TABS
-    ## =========================================================================== ##
-    source("ui-tab-inputdata.R",local=TRUE)$value,
-    ## =========================================================================== ##
-    ## Visualization TABS
-    ## =========================================================================== ##
-    source("ui-tab-samplegroupplots.R",local=TRUE)$value,
-    source("ui-tab-analysisres.R",local=TRUE)$value,
-    source("ui-tab-dotplot.R",local=TRUE)$value,
-    source("ui-tab-heatmap.R",local=TRUE)$value,
-    ## ============================================================================ ##
-    ## INFO TAB
-    ## ============================================================================ ##   
-    tabPanel("Info"),#end definitions of tabs, now footer
-    
-    ## ==================================================================================== ##
-    ## FOOTER
-    ## ==================================================================================== ##              
-    footer=p(hr(),p("ShinyApp created by ", 
-                    strong("{Jessica Minnier + Jiri Sklenar + Jonathan Nelson}")," of ",align="center",width=4),
-                    p(strong("Knight Cardiovascular Institute, Oregon Health & Science University"),align="center",width=4),
-                    p(strong("Copyright (C) 2014-2016, code licenesed under GPLv3"),align="center",width=4
-    )
-    )
-    
-    ## ==================================================================================== ##
-    ## end
-    ## ==================================================================================== ## 
-  ) #end Navbar
-#)#end shiny UI
+navbarPage(
+  fluid = TRUE,theme = "bootstrap.min.united.css",
+  #United theme from http://bootswatch.com/
+  #customHeaderPanel(title="START: RNAseq Analysis and Visualization Resource"),#img(src="KCardio_CMYK_4C_pos_small.jpg",height=50,width= 92,align="right")	,
+  title="START: RNAseq Analysis and Visualization Resource", 
+  ## =========================================================================== ##
+  ## DOWNLOAD DATA TABS
+  ## =========================================================================== ##
+  source("ui-tab-inputdata.R",local=TRUE)$value,
+  ## =========================================================================== ##
+  ## Visualization TABS
+  ## =========================================================================== ##
+  source("ui-tab-samplegroupplots.R",local=TRUE)$value,
+  source("ui-tab-analysisres.R",local=TRUE)$value,
+  source("ui-tab-dotplot.R",local=TRUE)$value,
+  source("ui-tab-heatmap.R",local=TRUE)$value,
+  ## ============================================================================ ##
+  ## INFO TAB
+  ## ============================================================================ ##   
+  tabPanel("Info"),#end definitions of tabs, now footer
+  
+  ## ==================================================================================== ##
+  ## FOOTER
+  ## ==================================================================================== ##              
+  footer=p(hr(),p("ShinyApp created by ", strong("{Jessica Minnier + Jiri Sklenar + Jonathan Nelson}")," of ",align="center",width=4),
+           p(("Knight Cardiovascular Institute, Oregon Health & Science University"),align="center",width=4),
+           p(("Copyright (C) 2014-2016, code licenesed under GPLv3"),align="center",width=4),
+           p(("Code available on Github:"),a("https://github.com/jminnier/STARTapp",href="https://github.com/jminnier/STARTapp"),align="center",width=4)
+  )
+  
+  ## ==================================================================================== ##
+  ## end
+  ## ==================================================================================== ## 
+) #end Navbar
+
